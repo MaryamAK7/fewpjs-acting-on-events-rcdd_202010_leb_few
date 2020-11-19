1c1e4c0d1515630a4b72c30917318661ec3f4e4c
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-11-19 16:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function moveDodgerRight(){
   let rightNumbers= dodger.style.right.replace("px","");
     let right= parseInt(rightNumbers, 10);
     if(right > 0){
-    dodger.style.right= `${right - 1}px`;
+    dodger.style.right= `${right + 1}px`;
   }
 }
