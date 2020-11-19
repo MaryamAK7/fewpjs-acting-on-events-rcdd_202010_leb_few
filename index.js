b654ce05740eaf79627b2f67cd9c3ffb2f516b77
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-11-19 16:07:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+let dodger= document.getElementById("dodger");
+document.addEventListener("keydown",function(e){
+  if(e.key==="ArrowRight"){
+    let rightNumbers= dodger.style.right.replace("px","");
+    let right= parseInt(rightNumbers, 10);
+    if(right > 0){
+    dodger.style.right= `${right - 1}px`;
+  }
+  }
+})
