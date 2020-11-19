74de0acbbaecd7f06003af26c2e3b53155ecd795
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-11-19 16:10:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let dodger= document.getElementById("dodger");
 document.addEventListener("keydown",function(e){
   if(e.key==="ArrowLeft"){
-   
+   moveDodgerLeft();
   }
   }
 )
@@ -16,14 +16,14 @@ function moveDodgerLeft(){
 
 document.addEventListener("keydown",function(e){
   if(e.key==="ArrowRight"){
-    let rightNumbers= dodger.style.right.replace("px","");
-    let right= parseInt(rightNumbers, 10);
-    if(right > 0){
-    dodger.style.right= `${right - 1}px`;
-  }
+    moveDodgerRight();
   }
 })
 
 function moveDodgerRight(){
-  
+  let rightNumbers= dodger.style.right.replace("px","");
+    let right= parseInt(rightNumbers, 10);
+    if(right > 0){
+    dodger.style.right= `${right - 1}px`;
+  }
 }
